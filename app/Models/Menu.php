bc0f40e56d9563db7f94c10756c220515f3cd6d9
Stephen Nkwocha <chinedustevenkwocha@gmail.com>
2020-05-19 11:05:20 +0100

    Created basic models

---
+++ app/Models/Menu.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Menu extends Model
+{
+    protected $primaryKey = 'order_id';
+
+
+    const CREATED_AT = 'order_created_at';
+    const UPDATED_AT = 'order_updated_at';
+}
